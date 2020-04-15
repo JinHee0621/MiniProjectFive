b3908a7cf7218aa2ce014cc4e370f4647f64a314
kimin <kimin@DESKTOP-NVTK22F>
2020-04-15 15:06:54 +0900

    엔딩씬 추가
    엔딩씬에 필요한 finish1, finish2, finish3 이미지 추가하고 GameObject패키지 밑에 EndingScene
    클래스 만듦. GameWindow에 StartEnding메소드는 뺐어요

---
+++ src/com/kh/mini/model/gameObject/EndingScene.java
@@ -0,0 +1,71 @@
+package com.kh.mini.model.gameObject;
+
+import java.awt.Graphics;
+import java.awt.MouseInfo;
+import java.awt.PointerInfo;
+import java.awt.event.MouseEvent;
+
+import com.kh.mini.controller.KeyManager;
+import com.kh.mini.model.vo.ImageClass;
+import com.kh.mini.view.GameWindow;
+
+public class EndingScene extends BaseScene {
+
+   ImageClass bg;
+
+   PointerInfo pointerInfo;
+
+   GameWindow gw;
+   
+   int cnt = 0;
+
+   public EndingScene(GameWindow gw) {
+      this.gw = gw;
+   }
+
+   @Override
+   public void init() {
+
+      bg = new ImageClass(); //이미지 클래스 할당
+      bg.Init("images\\titleImages\\finish1.png");
+      bg.setIsOn(true);
+      bg.setPosition(0, 0);
+      
+   }
+   
+   @Override
+   public void release() {
+      // TODO Auto-generated method stub
+   }
+   
+   @Override
+      public void update() {
+    
+         pointerInfo   = MouseInfo.getPointerInfo();
+
+         for(int i = 0; i < 5; i++) {
+            if((KeyManager.Instance().onceMouseClicked(MouseEvent.BUTTON1))&&((pointerInfo.getLocation().x >= 217 && pointerInfo.getLocation().x <= 1625) && (pointerInfo.getLocation().y >= 32 &&pointerInfo.getLocation().y <= 922))){
+               cnt += 1;
+               
+               switch(cnt) {
+               case 1 : bg.changeImage("images\\titleImages\\finish2.png");   break;
+               case 2 : bg.changeImage("images\\titleImages\\finish3.png");   break;
+               default : cnt--;            
+               }
+
+            }
+
+         }
+
+      }
+
+   //   if((pointerInfo.getLocation().x >= 217 && pointerInfo.getLocation().x <= 1625 ) && 
+   //      (pointerInfo.getLocation().y >= 32 &&pointerInfo.getLocation().y <= 922 )) {
+   //   bg.changeImage("images\\titleImages\\finish2.png");
+
+   @Override
+   public void render(Graphics g) {
+      bg.render(g);
+   }
+
+}
