7f08b490180c67d04ed713e1fd1654dc9e9c4478
1004miso <62384024+1004miso@users.noreply.github.com>
2020-04-15 15:08:41 +0900

    OpeningScene, 사진5장추가
    OpeningScene, 사진5장추가

---
+++ src/com/kh/mini/model/gameObject/OpeningScene.java
@@ -0,0 +1,75 @@
+package com.kh.mini.model.gameObject;
+
+import java.awt.Graphics;
+import java.awt.MouseInfo;
+import java.awt.PointerInfo;
+import java.awt.event.MouseEvent;
+
+import com.kh.mini.controller.KeyManager;
+import com.kh.mini.model.vo.ImageClass;
+import com.kh.mini.view.Finish;
+import com.kh.mini.view.GameWindow;
+import com.kh.mini.view.Join;
+
+public class OpeningScene  extends BaseScene{
+   ImageClass bg;
+   PointerInfo pointerInfo;
+   GameWindow gw;
+   int cnt = 0;
+   
+   public OpeningScene(GameWindow gw) {
+      this.gw = gw;
+   }
+
+
+   @Override
+   public void init() {
+      //이미지를 초기화 하고 위치를 정한다.
+
+      //배경 이미지 클래스 새로 할당
+      bg = new ImageClass();
+      bg.Init("images\\titleImages\\start1.png");
+      bg.setIsOn(true);
+      bg.setPosition(0, 0);
+
+   }
+
+
+   @Override
+   public void release() {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void update() {
+      pointerInfo   = MouseInfo.getPointerInfo();
+
+      for(int i = 0; i < 5; i++) {
+         if((KeyManager.Instance().onceMouseClicked(MouseEvent.BUTTON1))&&((pointerInfo.getLocation().x >= 217 && pointerInfo.getLocation().x <= 1625) && (pointerInfo.getLocation().y >= 32 &&pointerInfo.getLocation().y <= 922))){
+            cnt += 1;
+
+            switch(cnt) {
+            case 1 : bg.changeImage("images\\titleImages\\start2.png");   break;
+            case 2 : bg.changeImage("images\\titleImages\\start3.png");   break;
+            case 3 : bg.changeImage("images\\titleImages\\start4.png");   break;
+            case 4 : bg.changeImage("images\\titleImages\\start5.png");   break;
+            default : cnt--;            
+            }
+
+         }
+
+      }
+
+   }
+
+
+
+   @Override
+   public void render(Graphics g) {
+      bg.render(g);
+
+   }
+
+
+}
